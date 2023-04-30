8b8f436d7a7752dabad034a929c3b6b1e7e5804e
Folarin Lawal <lawalfolarin6@gmail.com>
2023-04-30 18:21:43 +0100

    done with about section

---
+++ components/About.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import {GiMoebiusStar} from "react-icons/gi";
+
+function About() {
+  return (
+    <section className='overflow-hidden bg-gray-50  sm:grid  sm:grid-cols-2 px-8 md:px-12 lg:px-16 pb-12'>
+      <div className='p-4 flex items-center justify-center'>
+        <img
+          alt='Student'
+          src='https://pbs.twimg.com/media/Fua6YvLWIAIik9k?format=png&name=small'
+          className='min-h-56 w-full max-w-lg rounded-md object-cover sm:h-full'
+        />
+      </div>
+      <div className='flex flex-col gap-4 text-left'>
+        <h2 className='font-bold text-2xl md:text-5xl text-primary'>
+          What is JaredFromSubway?
+        </h2>
+        <p className='text-lg text-primary'>
+          IcyFloki is an ERC-20 token built on the Ethereum blockchain that is
+          inspired by the popular meme-inspired cryptocurrency, Shiba Inu.
+          However, instead of the regular Shiba Inu dog, IcyFloki is based on a
+          fictional Shiba Inu dog named Floki that lives in the icy wilderness.
+        </p>
+        <div className='flex flex-col gap-2'>
+          <div className='flex items-start gap-4'>
+            <div className='min-w-[4rem] h-[4rem] rounded-full bg-red-400 text-white flex items-center justify-center'>
+              <GiMoebiusStar className="text-3xl" />
+            </div>
+            <div>
+              <p className='text-primary text-lg'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+                dolore eius recusandae inventore sapiente rem saepe iusto.
+                Reiciendis autem animi unde. Numquam minima totam error eum!
+              </p>
+            </div>
+          </div>
+          <div className='flex items-start gap-4'>
+            <div className='min-w-[4rem] h-[4rem] rounded-full bg-emerald-400 text-white flex items-center justify-center'>
+              <GiMoebiusStar className="text-3xl" />
+            </div>
+            <div>
+              <p className='text-primary text-lg'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+                dolore eius recusandae inventore sapiente rem saepe iusto.
+                Reiciendis autem animi unde. Numquam minima totam error eum!
+              </p>
+            </div>
+          </div>
+          <div className='flex items-start gap-4'>
+            <div className='min-w-[4rem] h-[4rem] rounded-full bg-sky-400 text-white flex items-center justify-center'>
+              <GiMoebiusStar className="text-3xl" />
+            </div>
+            <div>
+              <p className='text-primary text-lg'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+                dolore eius recusandae inventore sapiente rem saepe iusto.
+                Reiciendis autem animi unde. Numquam minima totam error eum!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import NavBar from '@/components/NavBar'
 import Hero from '@/components/Hero';
+import About from '@/components/About';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,6 +13,7 @@ export default function Home() {
       <div className='flex min-h-screen pt-[calc(100vh)-200px] flex-col items-center justify-between bg-gray-50'>
         <Hero />
       </div>
+      <About />
     </div>
   );
 }
