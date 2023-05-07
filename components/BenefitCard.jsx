7807595d6fa53425dd4da29cc31470417d09231a
Mikail Rokib <atandarokib@gmail.com>
2023-05-07 15:47:04 +0100

    updated

--- components/BenefitCard.jsx
+++ components/BenefitCard.jsx
@@ -7,7 +7,6 @@ function BenefitCard({action}) {
     <article className='card text-white bg-black/10 border border-[#F5D544] flex flex-col items-center gap-4 p-8 py-12'>
       <Image width={100} height={100} src={icon} alt={title} />
       <h3 className='font-bold text-xl text-center'>{title}</h3>
-      <p className='text-center'>{body}</p>
     </article>
   );
 }
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,23 +1,26 @@
-import Link from 'next/link';
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 function Hero() {
   return (
-    <section id='home' className='overflow-hidden bg-transparent relative  flex flex-col items-center gap-8 pt-[10vh] md:pt-[20vh]  w-full'>
-      <div className='flex flex-col items-center justify-center pt-14'>
-        <h2 className='text-5xl md:text-[82px] leading-[100%] w-[90vw] max-w-7xl uppercase text-white font-black text-center '>
-          Maximize your Defi Potential <span className='text-lightGreen'>with JaredFromSubway</span>
+    <section
+      id="home"
+      className="overflow-hidden bg-transparent relative  flex flex-col items-center gap-8 pt-[10vh] md:pt-[20vh]  w-full"
+    >
+      <div className="flex flex-col items-center justify-center pt-14">
+        <h2 className="text-5xl md:text-[82px] leading-[100%] w-[90vw] max-w-7xl uppercase text-white font-black text-center ">
+          <span className="text-lightGreen">JaredFromSubway</span>
         </h2>
       </div>
-      <div className='flex items-center justify-center'>
+      <div className="flex items-center justify-center">
         <img
-          alt='Student'
-          src='/assets/logo.png'
-          className='min-h-56 w-[90vw] max-w-sm rounded-md object-cover sm:h-full'
+          alt="Student"
+          src="/assets/logo.png"
+          className="min-h-56 w-[90vw] max-w-sm rounded-md object-cover sm:h-full"
         />
       </div>
     </section>
   );
 }
 
-export default Hero
+export default Hero;
